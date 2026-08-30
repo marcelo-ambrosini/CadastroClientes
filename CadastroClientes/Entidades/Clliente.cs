f73a7d13b4b038f607688471e11c59829e9a57d1
Ambrosini <marceloambrosino61@gmail.com>
2026-08-30 20:49:19 -0300

    Commit inicial

---
+++ CadastroClientes/Entidades/Clliente.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace CadastroClientes.Entidades
+{
+    public class Clliente
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string Nome { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Telefone { get; set; } = string.Empty;
+
+        public string Cpf { get; set; } = string.Empty;
+
+        public  DateOnly DataNacimento { get; set; }
+
+        public DateTime DataHoraCadastro { get; set; } = DateTime.Now;
+
+        /// <summary>
+        /// Método para salvar do dados do cliente em arquivo TXT local.
+        /// </summary>
+        public void Salvardadosa()
+        {
+            try
+            {
+                //Crindo um arquivo na maquina local do usuario:
+                //true -> modo append,ou seja, os registros serão salvo no mesmo arquivo
+                var sw = new StreamWriter("c:\\temp\\clientes.txt", true);
+
+                //Escrevendo os dados do cliente no arquivo
+                sw.WriteLine(Id);
+                sw.WriteLine(Nome);
+                sw.WriteLine(Email);
+                sw.WriteLine(Telefone);
+                sw.WriteLine(Cpf);
+                sw.WriteLine(DataNacimento);
+                sw.WriteLine(DataHoraCadastro);
+                sw.WriteLine("*******");
+
+                //fechando o arquivo
+                sw.Close();
+
+                Console.WriteLine("\nDados gravados com ducesso");
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("\falha ao salvar os dados");
+            }
+
+        }
+    }
+}
---
+++ CadastroClientes/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Aula 01 - C# WebaDeveloper COTI Informática
+ * Data: 25/06/26
+ */
+
+using CadastroClientes.Entidades;
+using System.Threading.Channels;
+
+Console.WriteLine("\n*** CADASTRO DE CLIENTE ***\n");
+Console.WriteLine("Entre com os dados do cleinete:");
+
+//Crindo um abjeto para a classe Cliente 
+var Cliente = new Clliente();
+
+//Prenchendo os dados do cliente
+Console.Write("INFORME O NOME DO CLIENTE....:");
+Cliente.Nome = Console.ReadLine();
+
+Console.Write("INFORME O CPF.........");
+Cliente.Cpf = Console.ReadLine();
+
+Console.Write("INFORME O TELEFONE........");
+Cliente.Telefone = Console.ReadLine();
+
+Console.Write("INFORME O EMAIL.........");
+Cliente.Email = Console.ReadLine();
+
+Console.Write("IMFORME A DATA DE NASCIMENYO:");
+Cliente.DataNacimento = DateOnly.Parse(Console.ReadLine());
+
+//Imprimindo os dados 
+Console.WriteLine("\nDADOS DO CLIENTE:");
+Console.WriteLine("\tID..............:" + Cliente.Id);
+Console.WriteLine("\tNOME.............:" + Cliente.Nome);
+Console.WriteLine("\tCPF...............:" + Cliente.Cpf);
+Console.WriteLine("\tTELEFONE...........:" + Cliente.Telefone);
+Console.WriteLine("\tEMAIL................:" + Cliente.Email);
+Console.WriteLine("\tDATA NASCIMENTO .........." + Cliente.DataNacimento);
+Console.WriteLine("\tCADASTRO EM ...." + Cliente.DataHoraCadastro);
+
+//salvar os dados no aequivo
+Cliente.Salvardadosa();
+
+
+Console.WriteLine();
